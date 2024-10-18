65bab9b681498cf8eca5cabdcceb727e843c0944
Lucas Ribeiro <estg-lnr@tce.rs.gov.br>
2024-10-18 18:14:38 -0300

    esqueleto da 2.8

---
+++ ex_2_8.c
@@ -0,0 +1,13 @@
+/*
+8. solicite as notas de um aluno nas avaliações previstas no plano de ensino desta disciplina,
+calcule a sua média e informe se o aluno está aprovado ou reprovado com base nas notas
+obtidas, incluindo a recuperação. Use este programa para avaliar seu próprio desempenho
+na disciplina.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
