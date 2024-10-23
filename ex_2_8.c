e66025c23c5ea106eadfd0beac146fb7071aa5c9
Lucas Ribeiro <estg-lnr@tce.rs.gov.br>
2024-10-23 14:53:00 -0300

    2.9

--- ex_2_8.c
+++ ex_2_8.c
@@ -7,7 +7,45 @@ na disciplina.
 
 #include <stdio.h>
 
+char nota_para_conceito(float nota) {
+    char conceito = {0};
+
+    if (nota >= 9) {
+        conceito = 'A';
+    } else if (nota >= 7.5) {
+        conceito = 'B';
+    } else if(nota >= 6) {
+        conceito = 'C';
+    } else {
+        conceito = 'D';
+    }
+
+    return conceito;
+}
+
 int main() {
+    float notas[3];
+
+    for (int i = 0; i < 3; i++) {
+        printf("Nota da lista %d: ", i +1);
+        scanf("%f", &notas[i]);
+    }
+
+    float media = (notas[0] + notas[1] + notas[2]) / 3;
+
+    char conceito = nota_para_conceito(media);
+
+    float nota_recup = 0;
+    if (conceito == 'D') {
+        printf("Nota da Recuperação: ");
+        scanf("%f", &nota_recup);
+        media = (3 * nota_recup + 2 * media) / 5;
+        conceito = nota_para_conceito(media);
+    }
+
+    printf("Nota final: %.1f\n", media);
+    printf("Conceito: %c\n", conceito);
+
 
     return 0;
 }
